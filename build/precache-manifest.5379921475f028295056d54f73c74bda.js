5873c981b226ef40e1370799a9f8920da3622d5b
avant10 <62254939+avant10@users.noreply.github.com>
2020-03-20 11:37:15 +0800

    Add files via upload

---
+++ build/precache-manifest.5379921475f028295056d54f73c74bda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b795a3a968acda5ae9cf58db5b405b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f420d7f79f15384878cd",
+    "url": "/static/css/2.847f907d.chunk.css"
+  },
+  {
+    "revision": "0c9bb141b5ee59b112d8",
+    "url": "/static/css/main.cb4eb432.chunk.css"
+  },
+  {
+    "revision": "f420d7f79f15384878cd",
+    "url": "/static/js/2.37cb96d3.chunk.js"
+  },
+  {
+    "revision": "0c9bb141b5ee59b112d8",
+    "url": "/static/js/main.6a066bba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
